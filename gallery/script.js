b279cdd62d91d067480f2a1e0547ce06e2d5f8f2
Collin <rocket3989@gmail.com>
2018-11-07 13:45:45 -0600

    changed soso much

--- gallery/script.js
+++ gallery/script.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
-let template = fs.readFileSync('template.html', 'utf8').split('`');
+let template = fs.readFileSync('/template/template.html', 'utf8').split('`');
 
 let galleries = fs.readdirSync('.');
 
 for(index = galleries.length - 1;index >=0;index--){ //have to loop goofy so I can kill elements
-    if(!RegExp('^\.[^.]+$').test(galleries[index])) //filter out items that arent directories
+    if(!RegExp('^\.[^.]+$').test(galleries[index])||galleries[index]=="template") //filter out items that arent directories
         galleries.splice(index,1);
+
 }
 
 galleries.forEach(gallery=>{
@@ -36,7 +37,7 @@ galleries.forEach(gallery=>{
         if (err) throw err;
         console.log('file saved to ./'+gallery+'/index.html');
     });   
-    fs.copyFile ('style.css','./'+gallery+'/style.css',(err) => {  
+    fs.copyFile ('/template/style.css','./'+gallery+'/style.css',(err) => {  
         if (err) throw err;
         console.log('style.css copied');
     });   
